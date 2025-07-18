a79e4c5a2b5dbf62e643ce5fb463c660e9224af5
Matthania <komlanadjohmatthania@gmail.com>
2025-07-18 16:11:10 +0200

    Projet mis en place

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'collect',
-    'USER': 'root',
-    'PASSWORD': 'django',
-    'HOST': 'localhost',
-    'PORT': '3305',
-    'OPTIONS': {
-      'charset': 'utf8mb4',
-      'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        'NAME': 'collecte',
+        'USER': 'root',
+        'PASSWORD': 'hcpcnd',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+        'charset': 'utf8mb4',
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
     }
     }
 }
